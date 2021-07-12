687537546cd3a3f42e35bc8199f3d83b50440687
sjookwag <76030642+sjookwag@users.noreply.github.com>
2021-07-12 13:38:29 +0900

    Add files via upload

--- ESG/config.py
+++ ESG/config.py
@@ -4,11 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
   SECRET_KEY = os.urandom(32) #os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')  #os.environ.get('DATABASE_URL')
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')  #os.environ.get('DATABASE_URL')  
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
   MAIL_USERNAME = 'sjoo.kwag@gmail.com' #os.environ.get('EMAIL_USER')
   MAIL_PASSWORD = 'jimsjoo78445' #os.environ.get('EMAIL_PASS')
   MEDIA = os.path.join(basedir, '/media')
+  # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
   
--- ESG/models.py
+++ ESG/models.py
@@ -9,13 +9,15 @@ def load_user(user_id):
   return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+  __tablename__ = 'user'
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
   password = db.Column(db.String(60), nullable=False)
   posts = db.relationship('Post', backref='author', lazy=True)
-
+  models = db.relationship('Model', backref='author', lazy=True)
+  
   def get_reset_token(self, expires_sec=1800):
     s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
     return s.dumps({'user_id': self.id}).decode('utf-8')
@@ -33,22 +35,49 @@ class User(db.Model, UserMixin):
     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Model(db.Model):
+  __tablename__ = 'model'
+  id       = db.Column(db.Integer, primary_key=True)
+  modelname= db.Column(db.String(100))  
+  category = db.Column(db.Integer)
+  fdist    = db.Column(db.String(3))
+  param10  = db.Column(db.Float)
+  param20  = db.Column(db.Float)
+  sdist    = db.Column(db.String(3))
+  param11  = db.Column(db.Float)
+  param21  = db.Column(db.Float)
+  param31  = db.Column(db.Float)
+  user_id  = db.Column(db.Integer, db.ForeignKey('user.id'))
+  cates = db.relationship('Category')
+  dists = db.relationship('Distribution')
+  
+  def __repr__(self):
+    return f"Model('{self.modelname}', '{self.category}')"
+
+class Category(db.Model):
+  __tablename__ = 'category'
   id = db.Column(db.Integer, primary_key=True)
-  riskname = db.Column(db.String(100), nullable=False)  
-  category = db.Column(db.Integer, nullable=False)
-  fdist    = db.Column(db.Integer, nullable=False)
-  param10  = db.Column(db.Float, nullable=True)
-  param20  = db.Column(db.Float, nullable=True)
-  sdist    = db.Column(db.Integer, nullable=False)
-  param11  = db.Column(db.Float, nullable=True)
-  param21  = db.Column(db.Float, nullable=True)
-  param31  = db.Column(db.Float, nullable=True)
-  user_id  = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  name = db.Column(db.String(100), nullable=False)  
+  description = db.Column(db.String(100), nullable=True)
+  abbr = db.Column(db.String(3), db.ForeignKey('model.category'), nullable=False)    
+  # models = db.relationship('Model', backref='category', lazy=True)
 
   def __repr__(self):
-    return f"Model('{self.riskname}', '{self.category}')"
+    return f"Category('{self.name}', '{self.description}')"
 
+class Distribution(db.Model):
+  __tablename__ = 'distribution'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False)  
+  type = db.Column(db.String(10), nullable=True)
+  params = db.Column(db.Integer, nullable=False)
+  abbr = db.Column(db.String(3), db.ForeignKey('model.fdist'), nullable=False)
+  # models = db.relationship('Model', backref='distribution', lazy=True)
+
+  def __repr__(self):
+    return f"Distribution('{self.name}', '{self.type}')"
+    
 class Post(db.Model):
+  __tablename__ = 'post'
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False)
   date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
